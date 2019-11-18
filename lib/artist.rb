4f453995b640ab40d3e53fce5cc88f0b6b363fe3
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-18 13:11:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,16 @@
-class Artist
+class Artist 
+  
+  
+  # a song should belong to an artist 
+  
+  # artist should have a name 
+  
+  # songs should have a title 
+  # songs should belon to an artist
+  # a song should be able to tell you the name of the artist
+  # song.artist.name 
+  
+  
+  
+  
+end 
