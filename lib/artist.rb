cae010143a0467ac450b002f142b8834ee49b757
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-18 13:33:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,10 +3,3 @@ class Artist
   attr_accessor :name
  
 end
-  
-  # artist should have a name 
-  
-  # songs should have a title 
-  # songs should belon to an artist
-  # a song should be able to tell you the name of the artist
-  # song.artist.name 
